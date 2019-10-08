a0cf3066d11bad16fa899887a9ab9cdb4e170e89
GuyBFrench <GuyBFrench@gmail.com>
2019-10-08 10:32:14 -0600

    Add First Practice
    Added First practice project using html and JavaScript together.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+window.alert("Hello, World!");
+
+var A = " -Uncle Iroh   ", B = "       \"While it is always best to believe in oneself, a little help from others can be a great blessing.\"", 
+    C = "       \"It is usually best to admit mistakes when they occur, and to seek to restore honor.\""
+
+var A = A.fontcolor("green"), B = B.fontcolor("blue"), C = C.fontcolor("red")
+
+document.write("       \"Pride is not the opposite of shame but its source. "
++ "True humility is the only antidote to shame.\" ");
+
+document.write(A);
+
+document.write(B);
+
+document.write(A);
+
+document.write(C);
+
+document.write(A);
