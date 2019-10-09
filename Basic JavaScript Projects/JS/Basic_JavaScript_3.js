bf42f44baae960d9ba20975c2bd815917a5efcf3
GuyBFrench <GuyBFrench@gmail.com>
2019-10-08 23:58:36 -0600

    add JavaScript Practices
    Added more practices utilizing JavaScript functions

--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -2,3 +2,18 @@ function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Tickle").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function my_Random() {
+    var tickle_Random = window.alert(Math.random() * 100);
+    document.getElementById("TopHat").innerHTML = "Random Number: " + tickle_Random;
+}
+
+function my_Pie() {
+    var tickle_Pie = Math.PI;
+    document.getElementById("HeckinBet").innerHTML = "PI: " + tickle_Pie;
+}
