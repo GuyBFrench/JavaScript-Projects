9cd604529a6eb92a525fe9dd896207306733859e
GuyBFrench <GuyBFrench@gmail.com>
2019-10-09 02:39:16 -0600

    Add Dictionary
    Added a JavaScript Dictionary practice

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,8 @@
+function my_Dictionary() {
+    var Favorite = {
+        favColor: "Green",
+        favNumber: 8,
+        favPlace: "Home",
+    };
+    document.getElementById("Favorite").innerHTML = Favorite.favColor;
+};
