aebbed88c94c48bde70ffb2cd85d111e0f546953
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2020-01-07 12:53:37 -0700

    Added  to Basic Javascript practice 10

--- Basic JavaScript Projects/JS/Basic_JavaScript_10.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -25,3 +25,23 @@ function constant_Function() {
     document.getElementById("Constant").innerHTML = "The cost of the " +
         Musical_Instrument.type + " was " + Musical_Instrument.price;
 }
+
+function array_Function(){
+    var Thing = [];
+    Thing[0] = "Lane"
+    Thing[1] = "Rode"
+    Thing[2] = "Strep"
+    document.getElementById("thing").innerHTML = "in this picture there is a " + Thing[0] +
+    ".";
+}
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "Red ",
+    description : function() {
+        return "the car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
