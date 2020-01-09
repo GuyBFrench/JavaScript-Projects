d8d731a8a8d697cdf400382832c2928277e914a2
GuyBFrench <55402501+GuyBFrench@users.noreply.github.com>
2020-01-09 11:54:18 -0700

    Fixed error in Pizza project

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -6,7 +6,7 @@ function getReceipt() {
 	for (var i = 0; i < sizeArray.length; i++) {
 		if (sizeArray[i].checked) {
 			var selectedSize = sizeArray[i].value;
-			text1 = text1+selectedSize+"<br>";
+			text1 = text1 + selectedSize + "<br>";
 		}
 	}
 	if (selectedSize === "Personal Pizza") {
@@ -19,12 +19,14 @@ function getReceipt() {
 		sizeTotal = 14;
 	} else if (selectedSize === "Extra Large Pizza") {
 		sizeTotal = 16;
+	} else if (selectedSize === "XXL Pizza") {
+		sizeTotal = 20;
 	}
 	runningTotal = sizeTotal;
-	console.log(selectedSize+" = $"+sizeTotal+".00");
-	console.log("size text1: "+text1);
-	console.log("subtotal: $"+runningTotal+".00");
-	getTopping(runningTotal,text1);
+	console.log(selectedSize+" = $" + sizeTotal + ".00");
+	console.log("size text1: " + text1);
+	console.log("subtotal: $" + runningTotal + ".00");
+	getTopping(runningTotal, text1);
 };
 
 function getTopping(runningTotal,text1) {
@@ -34,8 +36,8 @@ function getTopping(runningTotal,text1) {
 	for (var j = 0; j < toppingArray.length; j++) {
 		if (toppingArray[j].checked) {
 			selectedTopping.push(toppingArray[j].value);
-			console.log("selected topping item: ("+toppingArray[j].value+")");
-			text1 = text1+toppingArray[j].value+"<br>";
+			console.log("selected topping item: (" + toppingArray[j].value + ")");
+			text1 = text1 + toppingArray[j].value + "<br>";
 		}
 	}
 	var toppingCount = selectedTopping.length;
@@ -44,11 +46,11 @@ function getTopping(runningTotal,text1) {
 	} else {
 		toppingTotal = 0;
 	}
-	runningTotal = (runningTotal + toppingTotal);
-	console.log("total selected topping items: "+toppingCount);
-	console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
-	console.log("topping text1: "+text1);
-	console.log("Purchase Total: "+"$"+runningTotal+".00");
+	purchaseTotal = (runningTotal + toppingTotal);
+	console.log("total selected topping items: "+ toppingCount);
+	console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
+	console.log("topping text1: " + text1);
+	console.log("Purchase Total: " + "$" + runningTotal + ".00");
 	document.getElementById("showText").innerHTML=text1;
-	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+	document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$" + purchaseTotal + ".00" + "</strong></h3>";
 }
